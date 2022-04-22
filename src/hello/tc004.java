c9bc170e850b0be3c30cbf1b3e6b0941311a4ff1
nhlmk <nihalimek@gmail.com>
2022-04-22 16:31:26 +0300

    commit for tc003 step conf

---
+++ src/hello/tc004.java
@@ -0,0 +1,5 @@
+package hello;
+
+public class tc004 {
+    // step 1
+}
--- src/tc001/TestCase001.java
+++
@@ -1,6 +0,0 @@
-package tc001;
-
-public class TestCase001 {
-  // this is from GitHub.com
-}
-//I am gonna create a conflict from GitHub.com
